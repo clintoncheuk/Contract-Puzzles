9da4e8c31e8b5e02688e565bb2af257926dc5fe2
Clinton <clintoncky@gmail.com>
2023-05-07 14:40:39 +0800

    completed 5 tests

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,35 +1,41 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game3', function () {
+describe("Game3", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game3');
+    const Game = await ethers.getContractFactory("Game3");
     const game = await Game.deploy();
 
-    // Hardhat will create 10 accounts for you by default
-    // you can get one of this accounts with ethers.provider.getSigner
-    // and passing in the zero-based indexed of the signer you want:
-    const signer = ethers.provider.getSigner(0);
-
-    // you can get that signer's address via .getAddress()
-    // this variable is NOT used for Contract 3, just here as an example
-    const address = await signer.getAddress();
-
-    return { game, signer };
+    return { game };
   }
 
-  it('should be a winner', async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game } = await loadFixture(deployContractAndSetVariables);
 
     // you'll need to update the `balances` mapping to win this stage
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    const signer3 = ethers.provider.getSigner(2);
+
+    const addresses = [];
+
+    addresses.push(await signer1.getAddress());
+    addresses.push(await signer2.getAddress());
+    addresses.push(await signer3.getAddress());
+
+    await Promise.all([
+      game.connect(signer1).buy({ value: "200" }),
+      game.connect(signer2).buy({ value: "300" }),
+      game.connect(signer3).buy({ value: "100" }),
+    ]);
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(...addresses);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,34 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
-    // good luck
+    // brute force find address starting with 0x00
+    let signer, address;
+    do {
+      const pKey = ethers.Wallet.createRandom();
+      signer = new ethers.Wallet(pKey, ethers.provider);
+      address = await signer.getAddress();
+    } while (!address.startsWith("0x00"));
 
-    await game.win();
+    // fund the account with 1 ETH
+    const defaultSigner0 = ethers.provider.getSigner(0);
+    await defaultSigner0.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther("1.0"),
+    });
+
+    await game.connect(signer).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
